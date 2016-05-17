d6dade2f47a25f76b5a2d86172d92285110862ce
Ian Lenehan <ianlenehan@gmail.com>
2016-05-17 16:57:18 +1000

    ok finished I think

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   }, 2000);
 
   var $currentDiv;
-  var skills = ["JavaScript", "Ruby", "Rails", "ActiveRecord", "Backbone.js", "AJAX", "jQuery", "Regular Expressions", "Project Mangement", "Stakeholder Management", "Team Management", "Underscore.js"];
+  var skills = ["HTML", "CSS", "JavaScript", "Ruby", "Rails", "ActiveRecord", "Backbone.js", "Underscore.js", "AJAX", "jQuery", "Regular Expressions", "Project Mangement", "Stakeholder Management", "Team Management", "Team Management"];
   var aboutMe = ["I love Web Development.", "I love Photography.", "I love Game of Thrones.", "I mean, who doesn't?", "I play Baseball...", "and Golf.", "Oh and I do CrossFit.", "I love Problem Solving...", "and building things...", "on the web.", "Like this website.", "I'm a proud Irishman...", "who loves a cup of Barry's Tea...", "but I'm an Aussie too.", "Are you still with me?", "That's it for now.", "Thanks for stopping by!"];
   var counter = 0;
 
@@ -25,7 +25,7 @@ $(document).ready(function () {
     1: { image: "media/movienight.png", blurb: "Movie Night was built to solve that age-old problem of organising a movie night with your friends. Create your own group, create an event, signal your attendance, add the movie to the event and then rate the event afterwards!", title: 'Movie Night', weblink: "https://movienight-irl.herokuapp.com/", github: "https://github.com/ianlenehan/movienight" },
     2: { image: "media/tictactoe.png", blurb: "Tic Tac Toe was my first web app and was built during week three of the WDI course at General Assembly.", title: 'Tic Tac Toe', weblink: "http://ianlenehan.github.io/tic-tac-toe", github: "https://github.com/ianlenehan/tic-tac-toe" },
     3: { image: "media/glance.png", blurb: "Glance was built on Backbone during group project week. I combined with two others to develop this flash card gameified learning tool.", title: 'Glance', weblink: "http://wdi-glance.herokuapp.com/", github: "https://github.com/ianlenehan/flashcards" },
-    4: { image: "media/BTTF.png", blurb: "Yard Sale is my first mobile app, developed for my final project at General Assembly. The app is built on Rails using jQuery Mobile, jQuery, JavaScript, AJAX and of course, Ruby. The app is designed to make the selling and giving away of items amongst local residents much simpler.", title: 'Back to the Future', weblink: "http://ianlenehan.github.io/BTTF/", github: "https://github.com/ianlenehan/BTTF" },
+    4: { image: "media/BTTF.png", blurb: "After finishing Tic Tac Toe, I had some time and wanted to try out some of my newly acquired jQuery skills, so I built a fin little trivia game based on the Back to the Future movies.", title: 'Back to the Future', weblink: "http://ianlenehan.github.io/BTTF/", github: "https://github.com/ianlenehan/BTTF" },
   };
 
   /// basic open animation for photo
@@ -58,16 +58,16 @@ $(document).ready(function () {
   });
 
   /// display skills for skills div
-
-  $('#label-second').on('mouseenter', function() {
-    timerID = setInterval(displaySkills, 800);
-  });
+  //
+  // $('#label-second').on('mouseenter', function() {
+  //   timerID = setInterval(displaySkills, 800);
+  // });
 
   /// cancel skills timer
-
-  $('#label-second').on('mouseout', function () {
-    clearInterval(timerID);
-  });
+  //
+  // $('#label-second').on('mouseout', function () {
+  //   clearInterval(timerID);
+  // });
 
   /// start about me story on enter
 
@@ -82,6 +82,27 @@ $(document).ready(function () {
 
   $('#label-first').on('mouseout', function () {
     clearInterval(timerID);
+  });
+
+  $('#label-second').on('mouseenter', function () {
+    skillsTimer = setInterval(showSkills, 800);
+
+  });
+
+  var showSkills = function () {
+    $('#skills').html(skills[item]);
+    item ++;
+    if (item === skills.length) {
+      console.log('reached the end');
+      console.log('Item:', item, "Length:", skills.length);
+      $('#skills').fadeOut();
+      clearInterval(skillsTimer);
+      item = 0;
+    }
+  };
+
+  $('#label-second').on('mouseout', function () {
+    clearInterval(skillsTimer);
   });
 
   /// open portfolio page
@@ -104,7 +125,7 @@ $(document).ready(function () {
   /// function to open lightbox
 
   var displayLightbox = function() {
-    $('#portfolio-div').append('<div id="overlay"><h3 class="port-title">' + portfolio[counter].title + '</h3><p class="port-blurb">' + portfolio[counter].blurb + '</p><img class="lrg-img" src="' + portfolio[counter].image + '"><p class="port-blurb"><a href="' + portfolio[counter].weblink + '">Weblink</a>  |  <a href="' + portfolio[counter].github + '">Github</a>');
+    $('#portfolio-div').append('<div id="overlay"><h3 class="port-title">' + portfolio[counter].title + '</h3><p class="port-blurb">' + portfolio[counter].blurb + '</p><img class="lrg-img" src="' + portfolio[counter].image + '"><p class="links"><a id="weblink" href="' + portfolio[counter].weblink + '">Weblink</a>  |  <a id="github" href="' + portfolio[counter].github + '">Github</a></p>');
      $('#portfolio-div').append('<img class="close" src="media/close.png"></div>');
      $('#portfolio-div').append('<img id="forward" src="media/down.png">');
      $('#portfolio-div').append('<img id="back" src="media/down.png">');
@@ -137,15 +158,16 @@ $(document).ready(function () {
 
   $('body').on('click', '#forward', function () {
     console.log(counter);
-    if (counter >= 5 ) {
+    if (counter >= 4 ) {
       counter = 0;
     } else {
       counter ++;
     }
-    console.log(counter);
     $('.port-title').text(portfolio[counter].title);
     $('.port-blurb').text(portfolio[counter].blurb);
     $('.lrg-img').attr('src', portfolio[counter].image);
+    $('#weblink').attr('href', portfolio[counter].weblink);
+    $('#github').attr('href', portfolio[counter].github);
   });
 
   /// back
@@ -153,7 +175,7 @@ $(document).ready(function () {
   $('body').on('click', '#back', function () {
     console.log(counter);
     if (counter <= 0 ) {
-      counter = 5;
+      counter = 4;
     } else {
       counter --;
     }
@@ -163,9 +185,7 @@ $(document).ready(function () {
     $('.lrg-img').attr('src', portfolio[counter].image);
   });
 
-  var showSkills = function () {
 
-  };
 
 
   var displaySkills = function () {
