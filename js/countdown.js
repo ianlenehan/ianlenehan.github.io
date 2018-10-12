0c301df8e77e8e3214ceddc329630d24e8556ad9
Ian Lenehan <ian.lenehan@blake.com.au>
2018-10-12 13:09:04 +1100

    update countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 8, 2018 10:25:00").getTime();
+var countDownDate = new Date("Oct 26, 2018 21:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
