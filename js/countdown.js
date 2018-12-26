ea1cdc365f2250e9d71b80ddd1443267efc73770
Ian Lenehan <ianlenehan@gmail.com>
2018-12-26 23:14:01 +1100

    latest countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,7 @@
-var countDownDate = new Date("Sep 8, 2018 10:25:00").getTime();
+var countDownDate = new Date("Jan 23, 2019 04:40:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
-
   // Get todays date and time
   var now = new Date().getTime();
 
@@ -16,8 +15,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
