3beeaf3426c7aff179182b437271689d8f213328
Ian Lenehan <ian@yarno.com.au>
2019-03-18 12:42:57 +1100

    update timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 23, 2019 04:40:00").getTime();
+var countDownDate = new Date("Apr 5, 2019 17:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -14,13 +14,14 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown-days").innerHTML = days + "days";
+  document.getElementById("countdown-hours").innerHTML = hours + "hours";
+  document.getElementById("countdown-minutes").innerHTML = minutes + "minutes";
+  document.getElementById("countdown-seconds").innerHTML = seconds + "seconds";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
